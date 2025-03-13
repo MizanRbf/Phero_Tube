74a19f813a8195e903e37f896752dd7399c1fe3b
MizanRbf <mizanrbf@gmail.com>
2025-03-13 12:40:11 +0600

    project done

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function removeActiveClass(){
+  const activeButtons = document.getElementsByClassName("active");
+  for(let btn of activeButtons){
+  btn.classList.remove("active");
+  }
+}
 // Load Categories
 const loadCategories = () => {
 fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
@@ -5,10 +11,12 @@ fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
 .then(data => displayCategories(data.categories));
 }
 // Load Videos
-const loadVideos = () => {
-  fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+const loadVideos = (searchText = "") => {
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
   .then(res => res.json())
-  .then(data => displayVideos(data.videos));
+  .then(data => {
+    document.getElementById('btn-all').classList.add("active");
+    displayVideos(data.videos)});
 }
 // Load Category Videos
 const loadCategoryVideos = (id) => {
@@ -16,7 +24,19 @@ const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`
 
 fetch(url)
 .then(res => res.json())
-.then(data => displayVideos(data.category));
+.then(data => {
+  removeActiveClass();
+  const clickedButton = document.getElementById(`btn-${id}`);
+  clickedButton.classList.add('active');
+  displayVideos(data.category)});
+}
+// Load Video Details
+const loadVideoDetails = (videoId) => {
+const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
+
+fetch(url)
+.then(res => res.json())
+.then(data => displayVideoDetails(data.video));
 }
 
 // Display Categories
@@ -26,7 +46,7 @@ const displayCategories = (categories) => {
   for(let cat of categories){
     const categoryDiv = document.createElement('div');
     categoryDiv.innerHTML = `
-    <button onclick="loadCategoryVideos(${cat.category_id})" class="btn btn-sm">${cat.category}</button>`;
+    <button id="btn-${cat.category_id}" onclick="loadCategoryVideos(${cat.category_id})" class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>`;
     categoryContainer.append(categoryDiv);
   }
 }
@@ -46,10 +66,13 @@ videos.forEach((video) => {
   const videoCard = document.createElement('div');
   videoCard.innerHTML = `
   <div class="card bg-base-100 shadow-xl">
-      <figure>
+      <figure class="relative">
         <img class="object-cover w-full h-[150px]"
           src="${video.thumbnail}"
           alt="" />
+          <div class="absolute bottom-2 right-2 bg-black text-white px-2 text-sm">
+          3hrs 56 min ago
+          </div>
         </figure>
          <div class="p-4">
           <div class="flex">
@@ -59,16 +82,47 @@ videos.forEach((video) => {
           <div>
            <h2 class="card-title text-sm font-bold">${video.title}</h2>
            <div class="flex items-center">
-           <p class="flex gap-2">${video.authors[0].profile_name} <img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png"/></p>
+           <p class="flex gap-2">
+           ${video.authors[0].profile_name} 
+           ${video.authors[0].verified == true ? `<img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png"/>` : ``} 
+           </p>
            </div>
            <p>${video.others.views}</p>
           </div>
           </div>
+          <button onclick="loadVideoDetails('${video.video_id}')" class="btn btn-block mt-4 rounded-md">Show details</button>
     </div>`;
   videoContainer.append(videoCard);
 })
 }
+// Display Video Details
+const displayVideoDetails = (videoDetails) => {
+ document.getElementById('video_details').showModal();
+ const detailsContainer = document.getElementById('details-container');
+ detailsContainer.innerHTML = `
+ <div class="card bg-base-100 image-full w-96 shadow-sm">
+  <figure>
+    <img
+      src="${videoDetails.thumbnail}" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${videoDetails.title}</h2>
+    <p>${videoDetails.authors[0].profile_name}</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+ `;
+ 
+};
 loadCategories()
+
+// Search Input
+const searchInput = document.getElementById('search-input').addEventListener('keyup', (event) => {
+ const input = event.target.value;
+ loadVideos(input);
+})
 
 // {
 //   "category_id": "1003",
@@ -86,5 +140,3 @@ loadCategories()
 //       "views": "241K",
 //       "posted_date": ""
 //   },
-//   "description": "John Mulaney's 'Kid Gorgeous' has captured the hearts of many with 241K views. As a verified comedian, John delivers a masterclass in stand-up with clever anecdotes, quick wit, and relatable humor. This performance is a laugh-filled adventure through his unique take on life, politics, and pop culture."
-// }
